2d7b1c9e922326eb9b5f613926480001a9d5dad5
David Pruitt <daviddpruitt@gmail.com>
2018-04-13 10:34:20 -0600

    runTest cleanup

--- memMountain/runTest.c
+++ memMountain/runTest.c
@@ -18,16 +18,14 @@ runTest(uint64_t numIters, double *inputArray, double seedValue, uint64_t testAr
   register double dest_8  asm (FP_PREFIX"8" ); register double dest_9  asm (FP_PREFIX"9" ); 
   register double dest_10 asm (FP_PREFIX"10"); register double dest_11 asm (FP_PREFIX"11");
   register double dest_12 asm (FP_PREFIX"12"); register double dest_13 asm (FP_PREFIX"13"); 
-  register double dest_14 asm (FP_PREFIX"14"); //register double dest_15 asm (FP_PREFIX"15");
+  register double dest_14 asm (FP_PREFIX"14"); 
 
+  // Create an index into the array using a register
   register double *testArray asm(BASE_REG);asm volatile("":: "g"(testArray));
   
-  //double dest_1 = index_1, dest_2 = index_2, dest_3 = index_3, 
-  //       dest_4 = index_4, dest_5 = index_5, dest_6 = index_6, dest_7 = index_7;
   uint64_t iter;
   //uint64_t chunkNum;
-  // indexes for loading arrays in a single loop iteration
-
+  
   // initialize destinations 
   dest_0  = 1 -     seedValue; dest_1  = 1 + 1.2 * seedValue; 
   dest_2  = 1 +     seedValue; dest_3  = 1 - 1.2 * seedValue; 
@@ -36,7 +34,7 @@ runTest(uint64_t numIters, double *inputArray, double seedValue, uint64_t testAr
   dest_8  = 1 - 1.5 * seedValue; dest_9  = 1 + 1.6 * seedValue; 
   dest_10 = 1 + 1.5 * seedValue; dest_11 = 1 - 1.6 * seedValue; 
   dest_12 = 1 - 1.6 * seedValue; dest_13 = 1 + 1.7 * seedValue; 
-  dest_14 = 1 - .55 * seedValue; //dest_15 = 1 - 6 * seedValue;
+  dest_14 = 1 - .55 * seedValue; 
 
   __asm__ __volatile__("" :: "g" (dest_0));  __asm__ __volatile__("" :: "g" (dest_1));
   __asm__ __volatile__("" :: "g" (dest_2));  __asm__ __volatile__("" :: "g" (dest_3));
@@ -52,6 +50,7 @@ runTest(uint64_t numIters, double *inputArray, double seedValue, uint64_t testAr
   //index_4 = stride * 4; index_5 = stride * 5; index_6 = stride * 6; index_7 = stride * 7;
   //index_8 = stride * 8; index_9 = stride * 9; index_10 = stride * 10; index_11 = stride * 11;
   //index_12 = stride * 12; index_13 = stride * 13; index_14 = stride * 14; index_15 = stride * 15;
+ 
   //int printed = 0;
   //testArray = inputArray;
   asm("movq %0, %%rbx"::"g" (inputArray));
@@ -68,7 +67,6 @@ runTest(uint64_t numIters, double *inputArray, double seedValue, uint64_t testAr
   }
 
   // hack to keep compiler from optimizing away previous loop since ops aren't used
-  // note to self need extra registers
   /* bogusArray[0]  = dest_0;  bogusArray[1]  = dest_1;  bogusArray[2]  = dest_2;  bogusArray[3]  = dest_3; */
   /* bogusArray[4]  = dest_4;  bogusArray[5]  = dest_5;  bogusArray[6]  = dest_6;  bogusArray[7]  = dest_7; */
   /* bogusArray[8]  = dest_8;  bogusArray[9]  = dest_9;  bogusArray[10] = dest_10; bogusArray[11] = dest_11; */
